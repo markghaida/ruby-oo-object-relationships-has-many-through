4577b571778b8715e010dcf65e0b12c973658b71
Mark Ghaida <markghaida@gmail.com>
2020-11-20 08:45:24 -0500

    commit tree cc994389b10110cf258e1dbe8cde14786774e801

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
 class Waiter
 
+    attr_accessor :name, :age
+    @@all = []
+    def initialize(name, years)
+        @name = name 
+        @years = years
+        @@all << self 
+    end 
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def meals 
+        Meal.all.select {|meal| meal.waiter == self }
+    end 
+
+    def best_tipper
+        meals.max do |meal_a, meal_b|
+          meal_a.tip <=> meal_b.tip
+        end.customer 
+    end 
+      
 end
